a0b5b5636e7df0816b80f06b805378f97bf21b5e
JhK <readytostart94@gmail.com>
2024-02-19 20:18:18 +0900

    2024 02 19 jhk 커밋

--- src/main/java/com/livre/common/FrontController.java
+++ src/main/java/com/livre/common/FrontController.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.livre.utility.MyUtility;
+import com.oreilly.servlet.MultipartRequest;
 
 @WebServlet(urlPatterns = {"/Livre", "/xxx"},
 			initParams = {
@@ -23,6 +24,8 @@ public class FrontController extends HttpServlet{
 	private static final long serialVersionUID = 1L;
 	private String todolist = null; // 할 일을 명시해둔 장부 파일
 	private Map<String, SuperController> todolistMap = null;
+	
+	private String imageUploadWebPath = null ; 
 	
 	@Override
 	public void init(ServletConfig config) throws ServletException {
@@ -35,12 +38,42 @@ public class FrontController extends HttpServlet{
 		this.todolistMap = MyUtility.getTodolistMap(application.getRealPath(todolist));
 		System.out.println("todolist Map Size : " + todolistMap.size());
 		System.out.println(todolistMap.toString());
+		
+		String imsiPath = "assets";
+		this.imageUploadWebPath = application.getRealPath(imsiPath) ;
+		System.out.println("이미지 업로드 경로 : \n" + imageUploadWebPath);
 	}
 	
 	protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8"); // 한글 깨짐 방지
 		
 		String command = request.getParameter("command");
+		
+		// 파일 업로드시에는 request 내장 객체에서 직접 파라미터를 챙길수 없습니다.
+				// 이유는 폼 태그의 enctype="multipart/form-data" 속성 때문입니다.
+				// 이미지 관련 항목들을 Sring이 아닌 객체 형태로 다루어야 하기 때문입니다. 
+				if(command == null) {
+					System.out.println("file upload event invoked");
+					
+					MultipartRequest mr 
+						= MyUtility.getMultipartRequest(request, imageUploadWebPath);
+					
+					if(mr != null) {
+						// 파일 업로드인 경우에는 mr 객체를 이용해야만 파라미터 정보를 읽어올 수 있습니다. 
+						command = mr.getParameter("command") ;
+						
+						if(command.equals("prUpdate")) {
+							// 상품 정보 수정시 과거 이미지는 삭제해 주어야 합니다.
+							MyUtility.deleteOldImageFile(imageUploadWebPath, mr);
+						}
+						
+						// file upload object binding in request scope.
+						request.setAttribute("mr", mr);
+						
+					}else {
+						System.out.println("MultipartRequest object is null");
+					}
+				}
 		
 		System.out.println("command is [" + command + "]");
 		
